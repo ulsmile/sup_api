b4e2613e0c0b775fdf9f853c627901892dee73bc
sivertigo <sg.tak22@gmail.com>
2017-03-26 12:11:55 +0900

    removed noise in Gemfile

--- supapp/app/controllers/register_controller.rb
+++ supapp/app/controllers/register_controller.rb
@@ -1,17 +1,30 @@
 class RegisterController < ApplicationController
+  def index
+    @player = Players.all
+    render json: @player
+  end
   def show
 
     @player = Players.find_by(player_id: params[:player_id])
-    h = @player
-    bindng.pry
-    h.delete(:gender)
-
-    render :json => h
+    render :json => @player
   end
   
+  def new
+    @player = Players.new
+  end
+  def edit
+    @player = Players.find(player_id :params[:player_id])
+  end
   def create
+    @player = Players.new(params[:players])
+    if @player.save
+      redirect_to @person
+    else
+      render :action => 'new'
+    end
   end
 
   def update
+
   end
 end
